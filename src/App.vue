14c97f1d0e492d5f6cad0cdc9df560344305b3d9
lukasz-otowski <lukaszotowski@interia.pl>
2018-08-18 18:12:55 +0200

    updating bckground

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,14 @@ export default {
 </script>
 
 <style>
-
+    body {
+        background: url(/static/img/brain-bg.a46fac2.jpg);
+        background-size: cover;
+    }
+    .welcome .card{
+        border: 3px solid #124770;
+        border-radius: 50% 50% 50% 50% / 25% 25% 25% 25%;
+        -webkit-box-shadow: 0 0 10px 10px #0068b9;
+        box-shadow: 0 0 10px 10px #0068b9;
+    }
 </style>
