89004baf8179bc5332972ae550c211044fdabdcb
lukasz-otowski <lukaszotowski@interia.pl>
2018-08-22 11:47:03 +0200

    creating html css template

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="chat container">
-        <h2>Chat {{this.name}}</h2>
+        <div class="card">
+        <h2 class="center teal-text">Brain chat</h2>
+            <div class="card-content">
+                <ul class="messages">
+                    <li>
+                        <span class="teal-text">Name</span>
+                        <span class="grey-text text-darken-3">message</span>
+                        <span class="grey-text time">time</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-action">
+                <input type="text">
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'chat',
-        props:['name'],
+        props: ['name'],
         data() {
             return {
 
@@ -18,6 +32,24 @@
 </script>
 
 <style>
-
+    .chat {
+        margin-top: 40px;
+    }
+    .chat .card {
+        border: 3px solid #124770;
+        -webkit-box-shadow: 0 0 10px 10px #0068b9;
+        box-shadow: 0 0 10px 10px #0068b9;
+    }
+    .chat h2{
+        font-size: 2.6em;
+        padding-top: 20px;
+    }
+    .chat span{
+        font-size: 1.4em;
+    }
+    .chat .time{
+        display: block;
+        font-size: 1.2em;
+    }
 
 </style>
