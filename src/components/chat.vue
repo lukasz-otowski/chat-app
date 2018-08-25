954ae96fd6288293a1ecd6dc7e81bbaed66f547d
lukasz-otowski <lukaszotowski@interia.pl>
2018-08-25 23:38:16 +0200

    created auto-scrolling

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -3,7 +3,7 @@
         <div class="card">
         <h2 class="center teal-text">Brain chat</h2>
             <div class="card-content">
-                <ul class="messages">
+                <ul class="messages" v-chat-scroll >
                     <li v-for="message in messages" :key="message.id">
                         <span class="teal-text">{{message.name}}</span>
                         <span class="grey-text text-darken-3">{{message.content}}</span>
@@ -74,6 +74,19 @@ export default {
     .chat .time{
         display: block;
         font-size: .9em;
+    }
+    .messages {
+        max-height: 300px;
+        overflow: auto;
+    }
+    .messages::-webkit-scrollbar {
+        width: 3px;
+    }
+    .messages::-webkit-scrollbar-track {
+        background: #ddd;
+    }
+    .messages::-webkit-scrollbar-thumb {
+        background: #aaa;
     }
 
 </style>
