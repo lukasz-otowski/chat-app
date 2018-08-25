670176f75ab16a1f90d91ad5f923a31c49a0550c
lukasz-otowski <lukaszotowski@interia.pl>
2018-08-25 10:20:56 +0200

    corrected firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCBE2vGduV9dJct_CqSF9MNBR4DBfPz8_g",
-    authDomain: "brain-chat-39fec.firebaseapp.com",
-    databaseURL: "https://brain-chat-39fec.firebaseio.com",
-    projectId: "brain-chat-39fec",
-    storageBucket: "brain-chat-39fec.appspot.com",
-    messagingSenderId: "620519114410"
-};
-firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA2qGi1_pozXK4YvLwf5B9xfLZReTMY4gk",
+    authDomain: "brain-chat-3431f.firebaseapp.com",
+    databaseURL: "https://brain-chat-3431f.firebaseio.com",
+    projectId: "brain-chat-3431f",
+    storageBucket: "brain-chat-3431f.appspot.com",
+    messagingSenderId: "519551101488"
+  };
+
 const firebaseApp = firebase.initializeApp(config);
 firebaseApp.firestore().settings({timestampsInSnapshots: true})
 
